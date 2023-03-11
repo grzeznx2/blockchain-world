3963f0d2d450fe0bda6b028330bc3efdc18aebdc
grzeznx2 <grzeznx2@gmail.com>
2023-03-11 20:46:31 +0100

    Enable minting Land

--- src/Land.sol
+++ src/Land.sol
@@ -5,5 +5,25 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Land is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    struct Position {
+        uint256 x;
+        uint256 y;
+    }
+
+    mapping(uint256 => mapping(uint256 => bool)) private positions;
+    mapping(address => uint256) private landHolders;
+
     constructor() ERC721("LAND", "LAND"){}
+
+    function safeMint(uint256 _x, uint256 _y) external {
+        require(positions[_x][_y]==false,"Land: Land already taken");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        positions[_x][_y] = true;
+        landHolders[msg.sender] = tokenId;
+        _safeMint(msg.sender, tokenId);
+    }
 }
