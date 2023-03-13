f6e037212ebafd33662ed756999217eb40d018ac
grzeznx2 <grzeznx2@gmail.com>
2023-03-13 17:38:57 +0100

    Rename Land to Town

--- src/Town.sol
+++ src/Town.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Land is ERC721 {
+contract Town is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
-    enum LandType {
+    enum TownType {
         FIRST,
         SECOND,
         THIRD,
@@ -26,38 +26,38 @@ contract Land is ERC721 {
         uint256 building3;
     }
 
-    struct LandStats {
+    struct TownStats {
         uint256 id;
-        LandType landType;
+        TownType townType;
         BuildingLevels buildigLevels;
         Position position;
     }
 
     uint256 public GRID_SIZE = 999;
     mapping(uint256 => mapping(uint256 => bool)) private positions;
-    mapping(address => uint256) private landHolders;
-    mapping(uint256 => LandStats) private landById;
+    mapping(address => uint256) private townHolders;
+    mapping(uint256 => TownStats) private townById;
 
-    constructor() ERC721("LAND", "LAND"){}
+    constructor() ERC721("TOWN", "TOWN"){}
 
-    function safeMint(uint256 _x, uint256 _y, LandType landType) external {
-        require(_x <= GRID_SIZE && _x >= 0 && _y <= GRID_SIZE && _y >= 0, "LAND: invalid x or y");
-        require(positions[_x][_y]==false,"Land: Land already taken");
+    function safeMint(uint256 _x, uint256 _y, TownType townType) external {
+        require(_x <= GRID_SIZE && _x >= 0 && _y <= GRID_SIZE && _y >= 0, "TOWN: invalid x or y");
+        require(positions[_x][_y]==false,"Town: Town already taken");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         positions[_x][_y] = true;
-        landHolders[msg.sender] = tokenId;
-        landById[tokenId] = LandStats(
+        townHolders[msg.sender] = tokenId;
+        townById[tokenId] = TownStats(
             tokenId,
-            landType,
+            townType,
             BuildingLevels(0,0,0),
             Position(_x,_y)
         );
         _safeMint(msg.sender, tokenId);
     }
 
-    function getLandStatsById(uint256 landId) external view returns (LandStats memory){
-        _requireMinted(landId);
-        return landById[landId];
+    function getTownStatsById(uint256 townId) external view returns (TownStats memory){
+        _requireMinted(townId);
+        return townById[townId];
     }
 }
