fea6f8d04b261fff5ea5ac9a2b55702c02ec94f3
grzeznx2 <grzeznx2@gmail.com>
2023-03-13 17:37:01 +0100

    Add Land type and store Lands by id

--- src/Land.sol
+++ src/Land.sol
@@ -8,6 +8,13 @@ contract Land is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
+    enum LandType {
+        FIRST,
+        SECOND,
+        THIRD,
+        FOURTH
+    }
+
     struct Position {
         uint256 x;
         uint256 y;
@@ -21,30 +28,36 @@ contract Land is ERC721 {
 
     struct LandStats {
         uint256 id;
+        LandType landType;
         BuildingLevels buildigLevels;
         Position position;
     }
 
     uint256 public GRID_SIZE = 999;
     mapping(uint256 => mapping(uint256 => bool)) private positions;
-    mapping(uint256 => Position) private positionById;
     mapping(address => uint256) private landHolders;
+    mapping(uint256 => LandStats) private landById;
 
     constructor() ERC721("LAND", "LAND"){}
 
-    function safeMint(uint256 _x, uint256 _y) external {
+    function safeMint(uint256 _x, uint256 _y, LandType landType) external {
         require(_x <= GRID_SIZE && _x >= 0 && _y <= GRID_SIZE && _y >= 0, "LAND: invalid x or y");
         require(positions[_x][_y]==false,"Land: Land already taken");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         positions[_x][_y] = true;
-        positionById[tokenId] = Position(_x, _y);
         landHolders[msg.sender] = tokenId;
+        landById[tokenId] = LandStats(
+            tokenId,
+            landType,
+            BuildingLevels(0,0,0),
+            Position(_x,_y)
+        );
         _safeMint(msg.sender, tokenId);
     }
 
-    function getPositionById(uint256 landId) external view returns (Position memory){
+    function getLandStatsById(uint256 landId) external view returns (LandStats memory){
         _requireMinted(landId);
-        return positionById[landId];
+        return landById[landId];
     }
 }
